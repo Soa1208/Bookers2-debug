95c829f79715e3231ce815c9958d9c081a3aeabc
Sota3110 <144866058+Soa1208@users.noreply.github.com>
2024-02-02 09:11:48 +0900

    user編集機能の記述修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,15 @@ before_action :authenticate_user!
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render "edit"
     end
   end
 
